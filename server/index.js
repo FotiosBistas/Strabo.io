f3e81f22aa7847dd53b2a48ec66b141ce8738fd4
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2023-03-17 00:10:49 +0200

    Batch posts
    Batch posts that include a word tokenizer.

---
+++ server/index.js
@@ -0,0 +1,75 @@
+"use strict"
+const express = require('express')
+const https = require('https')
+const app = express();
+
+
+function log(text){
+    let time = new Date(); 
+    console.log("[" + time.toLocaleTimeString() + "] " + " " + text)
+}
+let result = null 
+//let result = https.createServer(app).listen(3000, () =>{
+//    log("Listening on port 3000 https server")
+//})
+
+
+
+app.use(express.static('public'));
+
+// parse url-encoded content from body
+app.use(express.urlencoded({ extended: true }));
+
+// parse application/json content from body
+app.use(express.json()) ;
+
+
+if(!result){
+    log("Could not create https server")
+    app.listen(3000, () =>{
+        log("Listening on port 3000 http server")
+    })
+}
+
+/**
+ * User will request for the model 
+ */
+app.get('/Model', (req,res) => {
+    
+})
+
+
+/**
+ * Receiving batches of translated and non-translated data 
+ */
+app.post('/Batch', (request,result) =>{
+    log("Received batch in http server"); 
+    const {uid,message} = request.body;
+  
+    // do something with the data
+    log({uid,message});
+    const words = CharacterTokenizer(message)
+    log(words)
+    // send a response
+    result.status(200).send('Received batch data');
+})
+
+/**
+ * Receives the message as input and tokenizes the words. 
+ * Removes all punctuation and lowercases all words. 
+ * @param {*} message 
+ * @returns the word array  
+ */
+function CharacterTokenizer(message) {
+    // Remove any punctuation inside the sentence and convert to lowercase
+    let transformed = message.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+
+    log(transformed)
+    // Split the sentence into an array of words using whitespace as the delimiter
+    const words = transformed.trim().split(/\s+/);
+
+    // Return the array of words
+    return words;
+}
+
+
