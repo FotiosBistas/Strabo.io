1b5e9efd453fbde16bd5d36cfc9f32845ecb8d0e
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2023-03-17 18:26:34 +0200

    proper handling of mongo db client

--- server/mongo_db_api/mongo.js
+++ server/mongo_db_api/mongo.js
@@ -5,32 +5,46 @@ function log(text){
     var time = new Date();
     console.log("[" + time.toLocaleTimeString() + "] " + text);
 }
-//
-//const options = {
-//    useNewUrlParse:true, 
-//    useUnifiedTopology:true, 
-//    poolSize: 10, 
-//    serverSelectionTimeoutMS: 5000,
-//};
+
+const options = {
+    useNewUrlParser:true, 
+    useUnifiedTopology:true, 
+    serverSelectionTimeoutMS: 5000,
+    serverApi: ServerApiVersion.v1
+};
 
 // Create a new MongoClient instance
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, options);
 
 module.exports = {
 
+    connectToDatabase: async function(){
+        try{
+            await client.connect();
+            log("Opened new connection successfully");
+        }catch(err){
+            log("Couldn't connect to database: " + err);
+        }    
+    },
+
+
+    closeConnection: async function(){
+        try{
+            await client.close();
+            log("Closed connection successfully");
+        }catch(err){
+            log("Couldn't close database connection");
+        }
+    },
 
     addStructToDatabase: async function(database_structs){
         try {
-            await client.connect();
-            log("Opened new connection successfully");
 
             const result = await client.db("UserData").collection("Translated-and-non").insertMany(database_structs);
             log("Documents inserted: " + result.insertedCount);
         } catch (err) {
             log("Documents were not inserted into the database error: " + err);
-        } finally {
-            client.close();
-        }
+        } 
     }  
 
 }
