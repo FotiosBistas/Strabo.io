bb0dbc93275f4ad283546ae5c614627eb9e2f74b
Fotis Bistas <83087431+FotiosBistas@users.noreply.github.com>
2023-04-04 01:17:49 +0300

    match score and bitmap

--- server/utils/dictionary.js
+++ server/utils/dictionary.js
@@ -7,6 +7,13 @@ function log(text){
 
 module.exports = {
 
+    /**
+     * After the dictionary is constructed for the specified time interval (e.g. a day)
+     * each new sample in a batch produces a bitmap to determine whether the sample can be 
+     * ignored and not inserted in the database.  
+     * @param {*} words the words contained inside the sample 
+     * @returns the bitmap for the specific sample.  
+     */
     createBitmaps: function(words){
         let word_bitmap = new Array(greek_dictionary.size).fill(0);
 
@@ -19,6 +26,23 @@ module.exports = {
         })
 
         return word_bitmap;  
+    },
+
+    /**
+     * Returns the match score for a specific bitmap. 
+     * E.g. if a sample has 3 matches over 5 it means it's a common occurence. 
+     * @param {*} bitmap an array of type [1,1,0,0,1] 
+     * @returns the percentage/match score 
+     */
+    extractMatchScore: function(bitmap){
+
+        let sum = 0; 
+        bitmap.forEach(sample => {
+
+            sum = sum + sample; 
+        });
+
+        return sum/length(greek_dictionary.size); 
     },
 
     /**
