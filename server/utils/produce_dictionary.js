6112535d26cf96128f5bde8755a0c7425802b07c
Fotis Bistas <83087431+FotiosBistas@users.noreply.github.com>
2023-04-04 19:21:02 +0300

    calculate weighted average and word occurence

--- server/utils/produce_dictionary.js
+++ server/utils/produce_dictionary.js
@@ -8,14 +8,23 @@ const schedule = require('node-schedule');
 const path = require('path');
 const parent_dir = path.join(__dirname, '..');
 const mongo_db_interactions = require(parent_dir + "\\mongo_db_api\\mongo.js");
-
+const batch_processing = require('./batch_processing.js'); 
 
 function log(text){
     let time = new Date(); 
     console.log("[" + time + "] " + " " + text)
 }
 
-const job = schedule.scheduleJob('*/20 * * * * *', createDailyDictionary)//TODO adjust times now runs at each midnight 
+const job = schedule.scheduleJob('*/2 * * * *', createDailyDictionary)//TODO adjust times now runs at each midnight 
+
+//weights used for the weighted average 
+const PREVIOUS_DATA_WEIGHT = 0.4; 
+const TODAYS_DATA_WEIGHT = 0.6; 
+
+//maps to count word occurences and weighted average
+const today_word_map = new Map(); 
+const rest_word_map = new Map(); 
+const weighted_average_map = new Map(); 
 
 
 async function createDailyDictionary(){
@@ -27,16 +36,98 @@ async function createDailyDictionary(){
     let previous_than_today_docs =  await mongo_db_interactions.retrieveData(
         "UserData", 
         "Translated_and_non", 
-        {_id:{$lt: mongo_db_interactions.objectIdWithTimestamp(today)}}); 
+        {timestamp:{$lt: "2023-04-04"}}); 
 
     let todays_docs = await mongo_db_interactions.retrieveData(       
-         "UserData", 
+        "UserData", 
         "Translated_and_non", 
-        {_id:{$lt: mongo_db_interactions.objectIdWithTimestamp(today)}})
+        {timestamp:{$eq:"2023-04-04"}})
 
-    docs.forEach(sample => {
-        
+   
+  
+    //decrypt words and count occurences
+    previous_than_today_docs.forEach(encryptedStruct => {
+        let decrypted_struct = batch_processing.decryptData(encryptedStruct);
+        decrypted_struct.translated_words.forEach(word => {
+            addCountToWord(word, rest_word_map);
+        });
     });
+
+    //decrypt words and count occurences
+    todays_docs.forEach(encryptedStruct => {
+        let decrypted_struct = batch_processing.decryptData(encryptedStruct);
+        decrypted_struct.translated_words.forEach(word => {
+            addCountToWord(word, today_word_map);
+        });
+    });
+
+
+    calculateWeightedAverages(today_word_map, rest_word_map); 
+
+    console.log("HI there");
+    //empty the maps 
+    today_word_map.clear(); 
+    rest_word_map.clear(); 
+    console.log("Hi there"); 
     
 
 }
+
+/**
+ * Increases the count for each word occurence for 
+ * the proper map  (today or previous period) 
+ * @param {*} word accepts the word we want to increase the count for 
+ * @param {*} map the previous day's or today's map  
+ */
+function addCountToWord(word, map){
+    //add one to the word occurences
+    if(map.has(word)){
+        map.set(word, map.get(word) + 1); 
+        log("Word: " + word + " occurences now are: " + map.get(word));
+        return 
+    }
+
+    map.set(word, 1); 
+    log("Word: " + word + " occurences now are: " + map.get(word)); 
+
+}
+
+/**
+ * Retrieves the count for each word from the wordmap 
+ * and calculates the weighted average. The weighted average 
+ * is inserted as a count in each word. 
+ * @param {*} map1 word map 1 (previous) order matters
+ * @param {*} map2 word map 2 (today) order matters 
+ * @returns the weighted average of each word
+ */
+function calculateWeightedAverages(map1, map2){
+
+    let larger_map = null; 
+    
+    //depending on the size of the maps 
+    //store the weighted average on the existing words 
+    if(map1.size >= map2.size){
+        for(const[key,value] of map1){
+            //if both maps have the key calculate the weighted average normally 
+            if(map2.has(key)){
+                let average = PREVIOUS_DATA_WEIGHT * map1.get(key) + TODAYS_DATA_WEIGHT * map2.get(key);
+                map1.set(key, average); 
+                continue; 
+            }
+            let average = PREVIOUS_DATA_WEIGHT * map1.get(key) + TODAYS_DATA_WEIGHT * 0;  
+            map1.set(key, average);
+        }
+    }else{
+        for(const[key,value] of map2){
+            //if both maps have the key calculate the weighted average normally 
+            if(map1.has(key)){
+                let average = PREVIOUS_DATA_WEIGHT * map1.get(key) + TODAYS_DATA_WEIGHT * map2.get(key);
+                map2.set(key, average); 
+                continue; 
+            }
+            let average = PREVIOUS_DATA_WEIGHT * 0 + TODAYS_DATA_WEIGHT * map2.get(key); 
+            map2.set(key, average);
+        }
+    }
+
+} 
