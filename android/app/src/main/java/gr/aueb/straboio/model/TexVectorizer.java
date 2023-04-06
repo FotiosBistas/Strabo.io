cbca2faa28e848df07095d5629f7a1b2e14ed4e5
Georgios E. Syros <geo2001s@gmail.com>
2023-04-06 13:38:31 +0300

    Ported @toumazatos's text vectorizer.

---
+++ android/app/src/main/java/gr/aueb/straboio/model/TexVectorizer.java
@@ -0,0 +1,108 @@
+package gr.aueb.straboio.model;
+
+import android.content.Context;
+
+import org.pytorch.Tensor;
+
+import java.util.List;
+import java.util.Map;
+
+public class TexVectorizer {
+
+    public enum Mode {
+        WORD, CHAR
+    }
+
+    private Mode mode;
+    private Vocabulary vocabulary;
+    private Context c;
+
+    public TexVectorizer(Context c, Mode mode) {
+        this.c = c;
+        this.mode = mode;
+        this.vocabulary = new Vocabulary();
+    }
+
+    public TexVectorizer(Context c, Mode mode, String pathToExistingVocab){
+        this(c, mode);
+        this.loadExistingVocab(pathToExistingVocab);
+    }
+
+    /**
+     * Loads existing vocabulary from external file:
+     * @param pathToExistingVocab the path to the external vocabulary.
+     */
+     private void loadExistingVocab(String pathToExistingVocab){
+        this.vocabulary.load(this.c, pathToExistingVocab);
+    }
+
+    /**
+     * Builds the vocabulary from a corpus of sentences. The words get encoded by
+     * count of appearances in the data.
+     * @param corpus : A list of sentences as strings.
+     */
+    public void buildVocab(List corpus){
+        buildVocab(corpus, 25000);
+    }
+
+    /**
+     * Builds the vocabulary from a corpus of sentences. The words get encoded by
+     * count of appearances in the data.
+     * @param corpus : A list of sentences as strings.
+     * @param MAX_SIZE : the maximum allowed size of the corpus.
+     */
+    public void buildVocab(List<String> corpus, final int MAX_SIZE){
+        TokenCounter tkcount = new TokenCounter();
+        this.vocabulary.insertToken("<pad>");
+
+        switch(mode){
+            case WORD:
+                // In the case of words, we remove punctuation and split on whitespaces
+                for (String line : corpus){
+                    // Remove punctuation
+                    line.replaceAll("["+Vocabulary.PUNCTUATION+"]", "");
+                    // Split the line in whitespaces to get the words & update counts
+                    tkcount.count(line.split("["+Vocabulary.WHITESPACE+"]"));
+                }
+                break;
+            case CHAR:
+                // Here we do not do any regularization, and split on every character.
+                for (String line : corpus)
+                    for (String character : line.split(""))
+                        tkcount.count(character);
+                break;
+        }
+        // Add the most frequent tokens to the vocabulary.
+        for(Map.Entry<String, Integer> entry : tkcount.mostCommon(MAX_SIZE)){
+            this.vocabulary.insertToken(entry.getKey());
+        }
+        // Add [SOS] token
+        this.vocabulary.insertToken("<s>");
+    }
+
+    /**
+     * Takes a sentence and returns its encoding, based on the vocabulary, to be used for inference.
+     * @param sequence The sentence to be encoded.
+     * @return Encoded sentence in form of a torch.Longtensor object.
+     */
+    public Tensor toTensor(String sequence){
+        String[] tokens = new String[0];
+        switch (this.mode){
+            case WORD:
+                tokens = sequence
+                        .replaceAll("["+Vocabulary.PUNCTUATION+"]", "")
+                        .split("["+Vocabulary.WHITESPACE+"]");
+                break;
+            case CHAR:
+                tokens = sequence.split("");
+                break;
+        }
+        long[] vectorized_tokens = new long[tokens.length];
+        for (int i=0; i<tokens.length; i++){
+            vectorized_tokens[i] = this.vocabulary.index(tokens[i]);
+        }
+
+        return Tensor.fromBlob(vectorized_tokens, new long[]{ vectorized_tokens.length });
+    }
+
+}
---
+++ android/app/src/main/java/gr/aueb/straboio/model/Vocabulary.java
@@ -0,0 +1,101 @@
+package gr.aueb.straboio.model;
+
+import android.content.Context;
+import android.util.Log;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Vocabulary {
+    public static final String PUNCTUATION = "!\"#$%&'()*+,-./:;<=>?@[\\\\]^_`{|}~";
+    public static final String WHITESPACE = " \\t\\n\\r\\x0b\\x0c";
+
+    private Map vocab;
+    private Map indexToToken;
+    private int currIndex;
+
+    public Vocabulary() {
+        this.vocab = new HashMap<String, Integer>();
+        this.indexToToken = new HashMap<Integer, String>();
+        currIndex = 0;
+    }
+
+    public int index(String token){
+        return (int) this.vocab.get(token);
+    }
+
+    public void insertToken(String token){
+        if(!this.vocab.containsKey(token)){
+            this.vocab.put(token, currIndex);
+            this.indexToToken.put(currIndex, token);
+            currIndex++;
+        }
+    }
+
+    public String token(int index){
+        return (String) this.indexToToken.get(index);
+    }
+
+    public void load(Context c, String vocab_file_name){
+        try {
+            String path = getAssetFilePath(c, vocab_file_name);
+            File f = new File(path);
+
+            // Create a Scanner object to read from the file
+            Scanner scanner = new Scanner(f);
+
+            // Loop all the lines of the file to a single string
+            StringBuilder text = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                text.append(line).append("\n");
+            }
+            // Close the Scanner object
+            scanner.close();
+
+            // Start extracting the keys and values:
+            // Split on '‽' (row delimiter).
+            String[] lines = text.toString().split("‽");
+            for(String line : lines){
+                try{
+                    // Split on '⁂' key-value delimiter.
+                    String[] parts = line.split("⁂");
+                    if(parts.length == 2){
+                        // Load vocabulary:
+                        this.vocab.put(parts[0], Integer.parseInt(parts[1]));
+                        this.indexToToken.put(Integer.parseInt(parts[1]), parts[0]);
+                    }
+                } catch(Exception e){
+                    Log.e("ERR_LOAD_VOCAB", "Error: "+e);
+                }
+            }
+        } catch (IOException e) {
+            Log.d("ERR_IO_VOCAB", "Error: "+e);
+        }
+    }
+
+    private static String getAssetFilePath(Context context, String assetName) throws IOException {
+        File file = new File(context.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
+        try (InputStream is = context.getAssets().open(assetName) ) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        }
+    }
+}
