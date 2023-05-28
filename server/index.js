a67952b21207f59347eaedf3e027fa0d3ffeac14
Georgios E. Syros <geo2001s@gmail.com>
2023-05-28 19:09:48 +0300

    Implemented model distribution

--- server/index.js
+++ server/index.js
@@ -43,11 +43,81 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 // parse application/json content from body
 app.use(express.json( {limit:'10mb'})) ;
 
+const SAVED_MODELS_DIRECTORY = './utils/python_scripts/saved_models';
+
+/**
+ * Returns the file name of the latest model.
+ */
+app.get('/peek', function(req, res) {
+    fs.readdir(SAVED_MODELS_DIRECTORY, function(err, files) {
+        if (err) {
+            res.status(500).send('INTERNAL SERVER ERROR');
+            log(err);
+            return;
+        }
+
+        let latest; // file name of the latest model
+        let latestTime = 0;
+
+        files.forEach(function(fileName) {
+            if (path.extname(fileName) === '.pt') {
+                const filePath = path.join(SAVED_MODELS_DIRECTORY, fileName);
+                const fileTime = fs.statSync(filePath).mtime.getTime();
+
+                if (!latest || fileTime > latestTime) {
+                    latest = fileName;
+                    latestTime = fileTime;
+                }
+            }
+        });
+
+        if (latest) {
+            res.json({'model_name': latest});
+            log('Latest model: ' + latest);
+        } else {
+            res.status(404).send('No models found.');
+            log('No models found.');
+        }
+    });
+});
+
+
 /**
  * User will request for the model 
+ * Sends the latest model to the user.
  */
 app.get('/Model', (req,res) => {
-    
+  fs.readdir(SAVED_MODELS_DIRECTORY, function(err, files) {
+    if (err) {
+        res.status(500).send('INTERNAL SERVER ERROR');
+        log(err);
+        return;
+    }
+
+    let latest; // file name of the latest model
+    let latestTime = 0;
+
+    files.forEach(function(fileName) {
+        if (path.extname(fileName) === '.pt') {
+            const filePath = path.join(SAVED_MODELS_DIRECTORY, fileName);
+            const fileTime = fs.statSync(filePath).mtime.getTime();
+
+            if (!latest || fileTime > latestTime) {
+                latest = fileName;
+                latestTime = fileTime;
+            }
+        }
+    });
+
+    if (latest) {
+        // Send the most recent file
+        res.sendFile(path.join(SAVED_MODELS_DIRECTORY, latest));
+        log('Sending latest model: ' + latest);
+    } else {
+        res.status(404).send('No models found.');
+        log('No models found.');
+    }
+  });
 })
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
